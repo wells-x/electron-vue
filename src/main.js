785cb45f3ffb0437d29c6c7675fe7b526939de49
wells <kang991005214@163.com>
2019-05-14 23:12:56 +0800

    's'

--- src/main.js
+++ src/main.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 
 Vue.use(iView);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,14 +2,14 @@
     <main>
         <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
             <FormItem prop="user">
-                <Input type="text" v-model="formInline.user" placeholder="Username">
+                <i-input type="text" v-model="formInline.user" placeholder="Username">
                     <Icon type="ios-person-outline" slot="prepend"></Icon>
-                </Input>
+                </i-input>
             </FormItem>
             <FormItem prop="password">
-                <Input type="password" v-model="formInline.password" placeholder="Password">
+                <i-input type="password" v-model="formInline.password" placeholder="Password">
                     <Icon type="ios-lock-outline" slot="prepend"></Icon>
-                </Input>
+                </i-input>
             </FormItem>
             <FormItem>
                 <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
